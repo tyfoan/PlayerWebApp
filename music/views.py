f3c2cacb39e7c2417adcf255657125efa6ae97dc
Александр Огурцов < >
2016-12-03 15:20:35 +0200

    Changed views to generic views

--- music/views.py
+++ music/views.py
@@ -1,32 +1,3 @@
-# from django.shortcuts import render, get_object_or_404
-# from .models import Album, Song
-#
-#
-# def index(request):
-#     all_albums = Album.objects.all()
-#     return render(request, 'music/index.html', {'all_albums': all_albums})
-#
-#
-# def detail(request, album_id):
-#     album = get_object_or_404(Album, pk=album_id)
-#     return render(request, 'music/detail.html', {'album': album})
-#
-#
-# def favorite(request, album_id):
-#     album = get_object_or_404(Album, pk=album_id)
-#     try:
-#         selected_song = album.song_set.get(pk=request.POST['song'])
-#     except (KeyError, Song.DoesNotExist):
-#         return render(request, 'music/detail.html', {
-#             'album': album,
-#             'error_message': 'You didnt select a valid song'
-#         })
-#     else:
-#         selected_song.is_favorite = True
-#         selected_song.save()
-#         return render(request, 'music/detail.html', {'album': album})
-
-
 from django.views import generic
 from .models import Album
 
